3e692285fc7e92f8ecb2c71590519fa7601043df
Vasily Glazkov <vvg.doc@yandex.ru>
2023-07-12 17:34:18 +0200

    added responsive styles for navbar

--- src/views/common/Header.jsx
+++ src/views/common/Header.jsx
@@ -2,7 +2,6 @@
 // const { locals } = require('express/lib/application');
 //
 // module.exports = function Header({ title, uid, isAdmin }) {
-//   console.log(locals);
 //   return (
 //     <header>
 //       <div><a href="/">AFMS</a></div>
@@ -15,82 +14,68 @@
 //       ) : (
 //         <nav>
 //           <ul>
-//             <li><a href="/">В магазин</a></li>
-//             <li><a href="/cart">Корзина</a></li>
-//             {uid ? (
+//             {!isAdmin && (
 //               <>
-//                 <li><a href="/orders">Заказы</a></li>
-//                 <li>
-//                   <form action="/logout" method="POST">
-//                     <button>Выйти</button>
-//                   </form>
-//                   {/* <a href="/logout"><button>Выйти</button></a> */}
-//                 </li>
+//                 <li><a href="/">В магазин</a></li>
+//                 <li><a href="/cart">Корзина</a></li>
+//                 {uid && (
+//                   <li><a href="/orders">Заказы</a></li>
+//                 )}
 //               </>
-//             ) : (
+//             )}
+//             {uid && !isAdmin ? (
 //               <li>
-//                 <a href="/login"><button>Войти</button></a>
+//                 <form action="/logout" method="GET">
+//                   <button>Выйти</button>
+//                 </form>
 //               </li>
+//             ) : (
+//               isAdmin ? (
+//                 <>
+//                   <li><a href="/admin/products">Управлять товарами</a></li>
+//                   <li><a href="/admin/orders">Управлять заказами</a></li>
+//                   <li><a href="/logout"><button>Выйти</button></a></li>
+//                 </>
+//               ) : (
+//                 <form action="/logout" method="GET">
+//                   <button>Выйти</button>
+//                 </form>
+//               )
 //             )}
 //           </ul>
 //         </nav>
 //       )}
-//       {isAdmin ? (
-//         <nav>
-//           <li><a href="/admin/products">Управлять товарами</a></li>
-//           <li><a href="/admin/orders">Управлять заказами</a></li>
-//         </nav>
-//       ) : ('')}
 //     </header>
 //   );
 // };
+
 const React = require('react');
-const { locals } = require('express/lib/application');
+const NavItems = require('./NavItems');
 
 module.exports = function Header({ title, uid, isAdmin }) {
   return (
-    <header>
-      <div><a href="/">AFMS</a></div>
-      {title === 'Авторизоваться' || title === 'Регистрация' || title === 'Ошибка сервера' ? (
-        <nav>
-          <ul>
-            <li />
-          </ul>
-        </nav>
-      ) : (
-        <nav>
-          <ul>
-            {!isAdmin && (
-              <>
-                <li><a href="/">В магазин</a></li>
-                <li><a href="/cart">Корзина</a></li>
-                {uid && (
-                  <li><a href="/orders">Заказы</a></li>
-                )}
-              </>
-            )}
-            {uid && !isAdmin ? (
-              <li>
-                <form action="/logout" method="GET">
-                  <button>Выйти</button>
-                </form>
-              </li>
-            ) : (
-              isAdmin ? (
-                <>
-                  <li><a href="/admin/products">Управлять товарами</a></li>
-                  <li><a href="/admin/orders">Управлять заказами</a></li>
-                  <li><a href="/logout"><button>Выйти</button></a></li>
-                </>
-              ) : (
-                <form action="/logout" method="GET">
-                  <button>Выйти</button>
-                </form>
-              )
-            )}
-          </ul>
-        </nav>
-      )}
-    </header>
+    <>
+      <link rel="stylesheet" href="/css/navigation.css" />
+      <header id="main-header">
+        <div id="logo"><a href="/">AFMS</a></div>
+        {title === 'Авторизоваться' || title === 'Регистрация' || title === 'Ошибка сервера' ? (
+          <nav>
+            <ul>
+              <li />
+            </ul>
+          </nav>
+        ) : (
+          <NavItems uid={uid} isAdmin={isAdmin} />
+        )}
+        <button id="mobile-menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+      </header>
+      <aside id="mobile-menu">
+        <NavItems uid={uid} isAdmin={isAdmin} />
+      </aside>
+    </>
   );
 };
---
+++ src/views/common/NavItems.jsx
@@ -0,0 +1,41 @@
+const React = require('react');
+
+module.exports = function NavItems({ isAdmin, uid }) {
+  return (
+    <nav>
+      <ul className="nav-items">
+        {!isAdmin && (
+          <>
+            <li><a href="/">В магазин</a></li>
+            <li><a href="/cart">Корзина</a></li>
+            {uid && (
+              <li><a href="/orders">Заказы</a></li>
+            )}
+          </>
+        )}
+        {uid ? (
+          <>
+            {isAdmin && (
+              <>
+                <li><a href="/admin/products">Управлять товарами</a></li>
+                <li><a href="/admin/orders">Управлять заказами</a></li>
+              </>
+            )}
+            <li>
+              <form action="/logout" method="GET">
+                <button>Выйти</button>
+              </form>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <a href="/login">Войти</a>
+            </li>
+            <li><a href="/register">Регистрация</a></li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
